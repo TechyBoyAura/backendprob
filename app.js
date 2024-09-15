4a045916ab89dda710dd4343e139eec0050fd393
TechyBoyAura <techavi786@gmail.com>
2024-09-15 20:28:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const JobListing = require("./models/jobListing.js"); // Import the JobListing model
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to MongoDB
+// mongoose
+//   .connect("mongodb://localhost:27017/jobjunction")
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.log(err));
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/jobjunction";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+// Route for rendering job listings with sector filter
+app.get("/", async (req, res) => {
+  const sector = req.query.sector || "IT"; // Default to IT if no sector is selected
+  console.log(await JobListing.find({}));
+  try {
+    const jobs = await JobListing.find({});
+    console.log("Jobs found:", jobs); // Add this to see what jobs are being found
+    res.render("jobs.ejs", { jobs });
+  } catch (err) {
+    console.log("Error fetching jobs:", err);
+  }
+});
+
+// Server
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
---
+++ jobListing.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const jobListingSchema = new mongoose.Schema({
+  company: String,
+  title: String,
+  sector: String, // e.g., IT, Consulting, Finance, Agriculture
+  location: String,
+  experience: String,
+  salary: String,
+  description: String,
+  datePosted: { type: Date, default: Date.now },
+  reviews: Number,
+  rating: Number,
+});
+
+const JobListing = mongoose.model("JobListing", jobListingSchema);
+
+module.exports = JobListing;
